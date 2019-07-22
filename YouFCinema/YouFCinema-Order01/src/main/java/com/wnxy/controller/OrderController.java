58630d66c00b25e2663b5f6ca06f3c21fbbe44e2
fz <2461728545@qq.com>
2019-07-22 19:14:58 +0800

    Order01微服务上传

--- YouFCinema/YouFCinema-Order01/src/main/java/com/wnxy/controller/OrderController.java
+++ YouFCinema/YouFCinema-Order01/src/main/java/com/wnxy/controller/OrderController.java
@@ -1,5 +1,32 @@
 package com.wnxy.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cinema.pojo.Order;
+import com.wnxy.service.IOrderService;
+
+@RestController
 public class OrderController {
-	
+	@Autowired
+	private IOrderService service;
+	/**
+	 * 	查询所有订单 
+	 */
+	@GetMapping("order/findAll")
+	public List<Order> findAll() {
+		List<Order> orders=service.findAll();
+		return orders;
+	}
+	/**
+	 * 	查询单个用户所有订单
+	 */
+	@GetMapping("order/findAllByUid")
+	public List<Order> findAllByUid(Integer uid) {
+		List<Order> orders=service.findOrdersByUid(uid);
+		return orders;
+	}
 }
