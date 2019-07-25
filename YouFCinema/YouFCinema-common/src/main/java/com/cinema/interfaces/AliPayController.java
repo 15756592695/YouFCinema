60b5e0840dc6d4f3963e32e54318c6dd34690638
Yanyu666666 <845572849@qq.com>
2019-07-25 20:07:27 +0800

    支付退款功能  和common中添加退款接口

--- YouFCinema/YouFCinema-common/src/main/java/com/cinema/interfaces/AliPayController.java
+++ YouFCinema/YouFCinema-common/src/main/java/com/cinema/interfaces/AliPayController.java
@@ -2,8 +2,11 @@ package com.cinema.interfaces;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
 
 @FeignClient(name="zhifubao")
 public interface AliPayController {
@@ -14,4 +17,10 @@ public interface AliPayController {
     @ResponseBody
     @GetMapping("/pay")
     String pay(@RequestParam("out_trade_no") String out_trade_no ,@RequestParam("subject") String subject,@RequestParam("total_amount") String total_amount);
+
+
+    @RequestMapping("/refund")
+    @ResponseBody
+    public void refund(@RequestParam("response") HttpServletResponse response, @RequestParam("ordernumber") String ordernumber, @RequestParam("payNumber") String payNumber, @RequestParam("total_money") String total_money);
+
 }
